095d54f9c5c557233417894e7614b8c67f4f1fb3
Patrick Krause <PKrause79@users.noreply.github.com>
2018-05-25 12:43:52 +0200

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -4,14 +4,10 @@ var seneca = require('seneca')({tag: 'demo'})
   .listen({port: '5003'});
 
 seneca.add('role:demo,cmd:hello', (msg, reply) => { 
-    console.log("");
-    console.log("request-message: " + JSON.stringify(msg));
     reply(null, {answer: "Hello world!"})
 });
 
 seneca.add('role:demo,cmd:queue', (msg, reply) => { 
-    console.log("");
-    console.log("request-message: " + JSON.stringify(msg));
     reply(null, {answer: "Hello world queued!"})
 });
 
